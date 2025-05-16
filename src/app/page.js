4bce67dc87170c052a1ec6c2bec2516f4d978dbe
mzubairr <mzubair5979@gmail.com>
2025-05-16 16:24:26 +0500

    home page complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { useRef } from "react"
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const user = useRef()
+  const router = useRouter()
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    router.push(`/profile/${user.current.value}`)
+  }
+
+  const userHandler = (e) => {
+    user.current.value = e.target.innerText;
+  }
+
   return (
     <>
       <div className="bg-img">
@@ -10,9 +25,9 @@ export default function Home() {
             <section id="user-section">
               <div>
                 <h1 className="text-4xl font-bold w-[40%]">A simpler way to explore your GitHub profile and repositories.</h1>
-                <form className="mt-7">
+                <form className="mt-7" onSubmit={formHandler}>
                   <div className="flex w-full max-w-sm items-center space-x-2">
-                    <Input id="username" className="bg-[#4E5E7E] placeholder:text-white" type="text" placeholder="Enter a github username..." />
+                    <Input id="username" ref={user} className="bg-[#4E5E7E] placeholder:text-white" type="text" placeholder="Enter a github username..." />
                     <Button className="bg-[#00C4F5] cursor-pointer" type="submit">View</Button>
                   </div>
                 </form>
@@ -20,9 +35,9 @@ export default function Home() {
               <div className="mt-8">
                 Amazing developers
                 <div className="flex items-center gap-3 mt-2">
-                  <Button className="bg-[#4E5E7E] cursor-pointer">mzubairr</Button>
-                  <Button className="bg-[#4E5E7E] cursor-pointer">mabdullahjs</Button>
-                  <Button className="bg-[#4E5E7E] cursor-pointer">shehza-d</Button>
+                  <Button onClick={userHandler} className="bg-[#4E5E7E] cursor-pointer">mzubairr</Button>
+                  <Button onClick={userHandler} className="bg-[#4E5E7E] cursor-pointer">mabdullahjs</Button>
+                  <Button onClick={userHandler} className="bg-[#4E5E7E] cursor-pointer">shehza-d</Button>
                 </div>
               </div>
             </section>
